5f2e5ee6e629d899612202de8fcce7030bd8f4e8
He Wang <r06631013@ntu.edu.tw>
2020-03-01 15:32:16 +0800

    Updates

---
+++ precache-manifest.4ba8e88ca0a28deaca9de3275344b568.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e008d5e772359fd9ee8089783344a765",
+    "url": "/react-gh-pages-demo/index.html"
+  },
+  {
+    "revision": "1f291e59faf2a5f38ada",
+    "url": "/react-gh-pages-demo/static/css/main.1ece31f1.chunk.css"
+  },
+  {
+    "revision": "6f7605c5fb792d6e02e3",
+    "url": "/react-gh-pages-demo/static/js/2.f419b6d3.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-gh-pages-demo/static/js/2.f419b6d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f291e59faf2a5f38ada",
+    "url": "/react-gh-pages-demo/static/js/main.7e17c03f.chunk.js"
+  },
+  {
+    "revision": "661614eb34ebab598a47",
+    "url": "/react-gh-pages-demo/static/js/runtime-main.996eb9f4.js"
+  },
+  {
+    "revision": "dc3c04df3391f478a28d7abb7c656134",
+    "url": "/react-gh-pages-demo/static/media/cancel.dc3c04df.svg"
+  },
+  {
+    "revision": "fce92e01408fe64ba6c802c13844bbfa",
+    "url": "/react-gh-pages-demo/static/media/edit.fce92e01.svg"
+  }
+]);
